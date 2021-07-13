2028c66fdb9d869a3847fa3f075a04a15bbff6a3
IND_Williams Jharil Azabache Sarmiento <xt3679@rimac.com.pe>
2021-07-13 01:25:45 -0500

    Fix route api service

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,29 +1,67 @@
-import db from '../../../libs/db'
+import db from '../../libs/db'
 import escape from 'sql-template-strings'
 
 export default async function handler(req, res) {
   const {
-    query: { id },
+    query: { category },
     method,
   } = req
 
   switch (method) {
     case 'GET':
       try {
-        const products = await db.query(escape`
-          SELECT product.*, category.title as catTitle, category.slug as catSlug,
-          product.price as original_price,
-          product.price - (product.price * (product.discount/100)) AS price
-          FROM product
-          LEFT JOIN category ON product.category_id = category.id
-        `)
+        let products = null
+
+        if(category){
+          const categoryData = await db.query(escape`
+            SELECT category.id
+            FROM category
+            where category.slug = ${category}
+          `)
+
+          const getAllCategories = await db.query(escape`
+            SELECT id
+              FROM category
+              WHERE FIND_IN_SET(ID, (
+                SELECT GROUP_CONCAT(Level SEPARATOR ',') FROM (
+                    SELECT @Ids := (
+                        SELECT GROUP_CONCAT(ID SEPARATOR ',')
+                        FROM category
+                        WHERE FIND_IN_SET(parent_id, @Ids)
+                    ) Level
+                    FROM category
+                    JOIN (SELECT @Ids := ${categoryData[0].id}) temp1
+                ) temp2
+            ))
+          `)
+          let catsChilds = [...getAllCategories.flatMap(e=> e.id), categoryData[0].id]
+
+          products = await db.query(escape`
+            SELECT product.*, category.title as catTitle, category.slug as catSlug,
+            product.price as original_price, 
+            product.price - (product.price * (product.discount/100)) AS price
+            FROM product
+            LEFT JOIN category ON product.category_id = category.id
+            where product.category_id IN (${catsChilds}) 
+            order by product.id desc
+          `)
+        } else {
+          products = await db.query(escape`
+            SELECT product.*, category.title as catTitle, category.slug as catSlug,
+            product.price as original_price ,
+            product.price - (product.price * (product.discount/100)) AS price
+            FROM product
+            LEFT JOIN category ON product.category_id = category.id
+            order by product.id desc
+          `)
+        }
 
         if (!products) {
-          return res.status(400).json({ success: false })
+          return res.status(400).json({ success: false, data: [] })
         }
         res.status(200).json({ success: true, data: products })
       } catch (error) {
-        res.status(400).json({ success: false })
+        res.status(400).json({ success: false, data: [] })
       }
       break
 
